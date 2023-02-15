4a4a34677636e88c41c712461d7afafe1d5721bf
SajjadAli11 <sajjad.uok@gmail.com>
2023-02-15 08:07:45 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+// var a, l, u;
+// u = parseInt(prompt("Please enter the length of diamond", "10"));
+// if(!isNaN(u)){
+//     a=u
+//     l=u
+//     for(i=1; i<=l; i++){
+            
+//             for(j=1; j<=a; j++){
+//                 document.write("&nbsp;");
+               
+//             }
+//             a--
+//             for(k=1; k<=i; k++){
+//                 document.write("*");
+//             }
+//             document.write("<br>");
+//     }
+//     a = u;
+//     for(i=1; i<=l; i++){
+
+//             for(k=1; k<=i; k++){
+//                 document.write("&nbsp");
+//             }
+//             for(j=1; j<=a; j++){
+//                 document.write("*");
+//             }
+//             a--
+//             document.write("<br>");
+//     }
+// }
+// else{
+//     document.write("Invalid input");
+// }
